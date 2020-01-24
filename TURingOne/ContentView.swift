f3de9caf9a64f49761da6d0cc260b3a7d1260848
Kyle Hobbs <kyle.hobbs01@gmail.com>
2020-01-24 12:09:02 -0700

    Initial Commit

---
+++ TURingOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TURingOne
+//
+//  Created by Kyle Hobbs on 1/24/20.
+//  Copyright © 2020 Kyle Hobbs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
